e3f7a30a5c5b20e9dfe62dc98b6f056188bf26de
michellejanosi <michelle.janosi@gmail.com>
2022-09-18 06:59:41 -0400

    Add post content function

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(data => {
-    const postsArr = data.slice(0, 5);
-    let html = "";
+const inputs = document.querySelectorAll('.input, .content');
+let postsArr = [];
 
-    for (let post of postsArr) {
-      html += `
+function renderPosts() {
+  let html = '';
+
+  for (let post of postsArr) {
+    html += `
         <h2>${post.title}</h2>
         <p>${post.body}</p>
         <hr>`;
-    }
+  }
 
-    const posts = document.querySelector('.posts');
-    posts.innerHTML = html;
-  })
+  const posts = document.querySelector(".posts");
+  posts.innerHTML = html;
+}
 
-  document.querySelector('.input-group').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const postTitle = document.querySelector('.input').value;
-    const postContent = document.querySelector('.content').value;
-    const data = {
-      title: postTitle,
-      content: postContent
-    }
-    console.log(data);
-  })
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((data) => {
+    postsArr = data.slice(0, 5);
+    renderPosts();
+  });
+
+document.querySelector(".input-group").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const postTitle = document.querySelector(".input").value;
+  const postContent = document.querySelector(".content").value;
+  const data = {
+    title: postTitle,
+    body: postContent,
+  };
+
+  if (postTitle && postContent) {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((post) => {
+        postsArr.unshift(post);
+        renderPosts();
+      });
+
+    inputs.forEach((input) => (input.value = ""));
+  }
+});
